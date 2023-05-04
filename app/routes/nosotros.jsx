7d83946a8525934e51022e333d02c823aefd486c
Ivan Enrique Vasquez Ramirez <ivanvasquez@Ivans-MacBook-Air.local>
2023-05-03 22:39:55 -0600

    Css a Nosotros y atributos meta

--- app/routes/nosotros.jsx
+++ app/routes/nosotros.jsx
@@ -1,7 +1,46 @@
+import imagen from '../../public/img/nosotros.jpg'
+import style from  '~/styles/nosotros.css'
 
+export function meta(){
+  return [
+    {
+      title: 'GuitarLa - Nosotros',
+      description: 'Venta de guitarras'
+    }
+  ]
+}
+
+export function links(){
+  return[
+    {
+      rel: 'stylesheet',
+      href: style,
+    },
+    {
+      rel: 'preload',
+      href: imagen,
+      as: 'image'
+    }
+  ]
+}
 const Nosotros = () => {
   return (
-    <div>Nosotros</div>
+    <main className="contenedor nosotros">
+      <h2 className="heading">Nosotros</h2>
+
+      <div className="contenido">
+        <img src={imagen} alt="imagen nosotros" />
+
+        <div>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc interdum ultricies accumsan. Ut vel maximus neque, id eleifend arcu. Integer pharetra accumsan eleifend. Ut hendrerit tortor non ligula sodales vehicula. In facilisis dolor maximus leo vehicula pretium. Curabitur sit amet bibendum quam.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc interdum ultricies accumsan. Ut vel maximus neque, id eleifend arcu. Integer pharetra accumsan eleifend. Ut hendrerit tortor non ligula sodales vehicula. In facilisis dolor maximus leo vehicula pretium. Curabitur sit amet bibendum quam.
+          </p>
+        </div>
+      </div>
+    </main>
   )
 }
 
