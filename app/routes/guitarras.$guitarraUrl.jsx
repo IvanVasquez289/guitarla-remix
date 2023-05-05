bb0e8411ed524103852d7c21f68b5ca8a76bce1f
Ivan Enrique Vasquez Ramirez <ivanvasquez@Ivans-MacBook-Air.local>
2023-05-05 00:10:39 -0600

    Mostrando la informacion de la guitarra

--- app/routes/guitarras.$guitarraUrl.jsx
+++ app/routes/guitarras.$guitarraUrl.jsx
@@ -1,24 +1,38 @@
 import { useLoaderData } from "@remix-run/react"
 import { getGuitarra } from "~/models/guitarras.server"
+import styles from '~/styles/tienda.css'
 
+export function links(){
+  return[
+    {
+      rel: 'stylesheet',
+      href: styles
+    }
+  ]
+}
 
 export async function loader({params}){
   // params es lo que tenemos en la url
   const {guitarraUrl} = params
-  
-  // { guitarraUrl: 'cobain' }  esto es un ejemplo de lo que se muestra en consola del lado del server
-  console.log(params)
-
   const guitarra = await getGuitarra(guitarraUrl)
-  // console.log(guitarra.data[0].attributes.nombre)
-
   return guitarra
 }
 
 const Guitarra = () => {
   const guitarra = useLoaderData()
+  const {nombre, imagen, descripcion, precio} = guitarra.data[0].attributes;
+  // console.log(imagen.data.attributes.url)
+
   return (
-    <div>Guitarra dinamico</div>
+    <main className="contenedor guitarra">
+      <img className="imagen" src={imagen.data.attributes.url} alt={`imagen de la guitarra ${nombre}`}/>
+
+      <div className="contenido">
+        <h3>{nombre}</h3>
+        <p className="texto">{descripcion}</p>
+        <p className="precio">${precio}</p>
+      </div>
+    </main>
   )
 }
 
