99aec1b03775658cd7773ec60a46a2a3a3080bf9
Ivan Enrique Vasquez Ramirez <ivanvasquez@Ivans-MacBook-Air.local>
2023-05-06 20:29:27 -0600

    Funcion para formatear fecha

--- app/components/post.jsx
+++ app/components/post.jsx
@@ -1,15 +1,17 @@
 import { Link } from "@remix-run/react";
+import {formatearFecha} from '~/utils/helper.js'
+
 const Post = ({post}) => {
 
   const {contenido, imagen, titulo, url, publishedAt} = post;
-
+formatearFecha
   console.log(post)
   return (
     <article className='post'>
         <img src={imagen.data.attributes.formats.small.url} alt={`imagen post ${titulo}`} />
         <div className="contenido">
             <h3>{titulo}</h3>
-            <p className="fecha">{publishedAt}</p>
+            <p className="fecha">{formatearFecha(publishedAt)}</p>
             <p className='resumen'>{contenido}</p>
             <Link className="enlace" to={`/posts/${url}`}>Leer post</Link>
         </div>
