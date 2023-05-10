a957a215da1ce9e4acd4c77a3f0abc470488caa2
Ivan Enrique Vasquez Ramirez <ivanvasquez@Ivans-MacBook-Air.local>
2023-05-09 23:24:40 -0600

    Agregando el contenido y css

--- app/routes/carrito.jsx
+++ app/routes/carrito.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import styles from '~/styles/carrito.css'
+
+export function links(){
+  return[
+    {
+      rel: 'stylesheet',
+      href: styles
+    }
+  ]
+}
+
+export function meta(){
+  return[
+    {
+      title: 'GuitarLA - Carrito',
+      description: 'Venta de guitarras, carrito de compras'
+    }
+  ]
+}
 
 const Carrito = () => {
   return (
-    <div>Carrito</div>
+    <main className='contenedor'>
+      <h1 className="heading">Carrito de compras</h1>
+
+      <div className="contenido">
+        <div>
+          <h2 className='carrito'>Articulos</h2>
+        </div>
+
+        <aside className="resumen">
+          <h3>Resumen total del pedido</h3>
+          <p>Total a pagar: $</p>
+        </aside>
+      </div>
+    </main>
   )
 }
 
